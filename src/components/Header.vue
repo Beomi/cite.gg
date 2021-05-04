073bccf192493ba1a1ac370c74726b2883f85e19
Junbum Lee <jun@beomi.net>
2021-05-04 11:16:46 +0900

    Remove unused button on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,20 +2,9 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid container">
-                <a class="navbar-brand" href="/">Find My Reference!</a>
-                <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <router-link class="navbar-brand" to="/">Find My Reference!</router-link>
                 <div class="d-flex">
-                    <a href="https://github.com/beomi">Beomi</a>
+                    Made by <a href="https://github.com/beomi">Beomi</a>
                 </div>
             </div>
         </nav>
